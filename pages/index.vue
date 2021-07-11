17972dde39f1d11ab6a59798af984564190c3ef2
k-iseri <water_dropwort@hotmail.com>
2021-07-11 19:53:54 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div>
+      カウント：{{ count }}
+    </div>
+    <div>
+      <button @click="increment">加算</button>
+      <button @click="reset">リセット</button>
+    </div>
+    <div>
+      カウントx2：{{ countx2 }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  computed: {
+    countx2: function() {
+      return this.count * 2;
+    }
+  },
+  methods: {
+    increment : function() {
+      this.count++;
+    },
+    reset: function() {
+      this.count = 0;
+    }
+  }  
+}
+</script>
